b985cd94349ba9e2710878d19b92444f0a0a9f80
Vitalii Tkachov <tkachov0703@gmail.com>
2023-10-02 23:41:13 +0300

    Refactoring

---
+++ src/components/Contacts/ContactForm.jsx
@@ -0,0 +1,59 @@
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = e => {
+    this.setState({ [e.currentTarget.name]: e.currentTarget.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    // const newContact = {
+    //   id: nanoid(),
+    //   name: this.state.name,
+    //   number: this.state.number,
+    // };
+    // this.setState(prevState => ({
+    //   contacts: [...prevState.contacts, newContact],
+    //   name: '',
+    //   number: '',
+    // }));
+
+    this.props.onSubmit(this.state);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="">
+          Name
+          <input
+            type="text"
+            name="name"
+            required
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label htmlFor="">
+          Number
+          <input
+            type="tel"
+            name="number"
+            required
+            value={this.state.number}
+            onChange={this.handleChange}
+          />
+        </label>
+        <button type="submit">Add Contact</button>
+      </form>
+    );
+  }
+}
+
+export default ContactForm;
