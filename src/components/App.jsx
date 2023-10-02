1a236bc750bd332e49c335c1a2b01075b144d20a
Vitalii Tkachov <tkachov0703@gmail.com>
2023-10-02 21:24:03 +0300

    Add Contact

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleChange = e => {
+    this.setState({ name: e.currentTarget.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const newContact = {
+      id: nanoid(),
+      name: this.state.name,
+    };
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+      name: '',
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            required
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Add Contact</button>
+        </form>
+        <h2>Contacts</h2>
+        <ul>
+          {this.state.contacts.map(contact => (
+            <li key={contact.id}>{contact.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
