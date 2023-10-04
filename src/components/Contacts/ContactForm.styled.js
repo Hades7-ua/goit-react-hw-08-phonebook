f61860a8dd381c97e122a77768de7f8bd84ebcf9
Vitalii Tkachov <tkachov0703@gmail.com>
2023-10-04 22:48:52 +0300

    fix

--- src/components/Contacts/ContactForm.styled.js
+++ src/components/Contacts/ContactForm.styled.js
@@ -13,7 +13,7 @@ export const Label = styled.label`
 
 export const Input = styled.input`
   width: 100%;
-  padding: 10px;
+  padding-top: 10px;
   margin-top: 5px;
   border: 1px solid #ccc;
   border-radius: 5px;
