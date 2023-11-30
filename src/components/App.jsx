893b44440b877585f591d0a42f037031656cc926
Vitalii Tkachov <tkachov0703@gmail.com>
2023-11-30 23:57:47 +0300

    App hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import ContactForm from './Contacts/ContactForm';
 import Filter from './Filter/FilterContacts';
 import ContactList from './ContactsList/ContactList';
 import { AppHeader, AppContent, AppContainer } from './App.styled';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     const getContacts = localStorage.getItem('contacts');
     const parsedContacts = JSON.parse(getContacts);
     if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
+      setContacts(parsedContacts);
     }
-    console.log(parsedContacts);
-  }
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  // componentDidMount() {
+  //   const getContacts = localStorage.getItem('contacts');
+  //   const parsedContacts = JSON.parse(getContacts);
+  //   if (parsedContacts) {
+  //     this.setState({ contacts: parsedContacts });
+  //   }
+  //   console.log(parsedContacts);
+  // }
 
-  addContact = ({ name, number }) => {
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (this.state.contacts !== prevState.contacts) {
+  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  //   }
+  // }
+
+  const addContact = ({ name, number }) => {
     const toLowerCaseName = name.toLowerCase();
-    const isNameThis = this.state.contacts.find(
+    const isNameThis = contacts.find(
       contact => contact.name.toLowerCase() === toLowerCaseName
     );
 
@@ -40,47 +50,41 @@ export class App extends Component {
         name,
         number,
       };
-      this.setState(prevState => ({
-        contacts: [...prevState.contacts, newContact],
-      }));
+      setContacts(prevContacts => [...prevContacts, newContact]);
     }
   };
 
-  handleFilterChange = e => {
-    this.setState({ filter: e.target.value });
+  const handleFilterChange = e => {
+    setFilter(e.target.value);
   };
 
-  getFilteredContacts = () => {
-    const { contacts, filter } = this.state;
+  const getFilteredContacts = () => {
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
-  handleDelete = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const handleDelete = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
   };
 
-  render() {
-    const { filter } = this.state;
-    const filteredContacts = this.getFilteredContacts();
+  const filteredContacts = getFilteredContacts();
 
-    return (
-      <AppContainer>
-        <AppHeader>Phonebook</AppHeader>
-        <AppContent>
-          <ContactForm onSubmit={this.addContact} />
-          <h2>Contacts</h2>
-          <Filter value={filter} onChange={this.handleFilterChange} />
-          <ContactList
-            contacts={filteredContacts}
-            onDeleteContact={this.handleDelete}
-          />
-        </AppContent>
-      </AppContainer>
-    );
-  }
-}
+  return (
+    <AppContainer>
+      <AppHeader>Phonebook</AppHeader>
+      <AppContent>
+        <ContactForm onSubmit={addContact} />
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={handleFilterChange} />
+        <ContactList
+          contacts={filteredContacts}
+          onDeleteContact={handleDelete}
+        />
+      </AppContent>
+    </AppContainer>
+  );
+};
 
 export default App;
