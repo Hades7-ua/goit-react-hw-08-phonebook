f4ccd27f1bb08b6415d4660187b4b26235dcc92b
Vitalii Tkachov <114616094+Hades7-ua@users.noreply.github.com>
2023-12-28 22:40:10 +0200

    correction of mistakes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,14 +24,9 @@ const LoginPage = lazy(() => import('../pages/Login'));
 const ContactsPage = lazy(() => import('../pages/Contacts'));
 
 export const App = () => {
-  // const dispatch = useDispatch();
-  // const loading = useSelector(selectIsLoading);
-  // const error = useSelector(selectError);
+
   const dispatch = useDispatch();
   const { isRefreshing } = useAuth();
-  // useEffect(() => {
-  //   dispatch(fetchContacts());
-  // }, [dispatch]);
 
   useEffect(() => {
     dispatch(refreshUser());
@@ -40,24 +35,15 @@ export const App = () => {
     <b>Refreshing user...</b>
   ) : (
     <Routes>
-      {/* <AppContainer>
-        {loading && !error && (
-          <b>The request is being processed, it will take a few seconds.....</b>
-        )}
-        <AppHeader>Phonebook</AppHeader>
-        <AppContent>
-          <ContactForm />
-          <h2>Contacts</h2>
-          <Filter />
-          <ContactList />
-        </AppContent>
-      </AppContainer> */}
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
         <Route
           path="/register"
           element={
-            <RestrictedRoute redirectTo="/contacts" component={<RegisterPage />} />
+            <RestrictedRoute
+              redirectTo="/contacts"
+              component={<RegisterPage />}
+            />
           }
         />
         <Route
