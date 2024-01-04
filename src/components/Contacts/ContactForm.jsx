3c24582e17b6825d2fc4a156306e255de89224e9
Vitalii Tkachov <114616094+Hades7-ua@users.noreply.github.com>
2024-01-04 12:41:40 +0200

    Form fix

--- src/components/Contacts/ContactForm.jsx
+++ src/components/Contacts/ContactForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Form, Input, Button, Label } from './ContactForm.styled';
+import { Form, Input, Button, Label, FormContact } from './ContactForm.styled';
 import { useSelector, useDispatch } from 'react-redux';
 import { addContact } from '../../redux/contacts/operations';
 import { selectContactsList } from '../../redux/contacts/selectors';
@@ -7,7 +7,7 @@ import { selectContactsList } from '../../redux/contacts/selectors';
 export const ContactForm = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(selectContactsList);
-  console.log(contacts)
+  console.log(contacts);
 
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
@@ -33,32 +33,36 @@ export const ContactForm = () => {
       alert(`${name} is already in contacts.`);
     } else {
       dispatch(addContact({ name: name, number: number }));
+      setNumber('');
+      setName('');
     }
   };
   return (
-    <Form onSubmit={handleSubmit}>
-      <Label htmlFor="name">
-        Name
-        <Input
-          type="text"
-          name="name"
-          required
-          value={name}
-          onChange={handleChange}
-        />
-      </Label>
-      <Label htmlFor="">
-        Number
-        <Input
-          type="tel"
-          name="number"
-          required
-          value={number}
-          onChange={handleChange}
-        />
-      </Label>
-      <Button type="submit">Add Contact</Button>
-    </Form>
+    <FormContact>
+      <Form onSubmit={handleSubmit}>
+        <Label htmlFor="name">
+          Name
+          <Input
+            type="text"
+            name="name"
+            required
+            value={name}
+            onChange={handleChange}
+          />
+        </Label>
+        <Label htmlFor="">
+          Number
+          <Input
+            type="tel"
+            name="number"
+            required
+            value={number}
+            onChange={handleChange}
+          />
+        </Label>
+        <Button type="submit">Add Contact</Button>
+      </Form>
+    </FormContact>
   );
 };
 
